8bd5e4961a557631cc933c5a8c8b3ad2aad0d26c
Tiago André <tiago.eugenio.andre@gmail.com>
2023-08-28 10:58:46 +0100

    project init and commit

---
+++ cypress.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://www.google.pt/',
+    specPattern: "**/*.feature",
+
+    // prefix async
+    async setupNodeEvents(on, config) {
+      const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin
+      const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+
+      // await here
+      await require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin(on, config)
+
+      on('file:preprocessor', createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      }));
+
+      // return any mods to Cypress
+      return config
+    },
+    "stepDefinitions": [
+      "cypress/integration/[filepath]/**/*.{js,ts}",
+      "cypress/integration/[filepath].{js,ts}",
+      "cypress/support/step_definitions/**/*.{js,ts}",
+    ]
+  },
+});
---
+++ cypress/support/step_definitions/test.js
@@ -0,0 +1,21 @@
+import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("I am in Google home page", () => {
+    cy.visit("/");
+});
+
+When("I type in {string}", (text) => {
+    cy.get("#APjFqb").type(`${text}`);
+});
+
+When("I click search button", () => {
+    cy.get("div.FPdoLc.lJ9FBc > center > input.gNO89b").click();
+});
+
+Then("I accept the cookies", () => {
+    cy.get("#L2AGLb > div").click();
+});
+
+Then("I should be redirected to the search page", () => {
+    cy.get("#result-stats").should('be.visible');
+});
